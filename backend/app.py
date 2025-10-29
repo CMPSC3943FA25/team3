051d8b406871d904f286704c09eb7d02cd5ec77a
ZacharyFarrell <162598563+ZacharyFarrell@users.noreply.github.com>
2025-10-29 18:11:45 -0500

    Create app.py

---
+++ backend/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+from flask_cors import CORS
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+@app.put("/api/plant")
+def create_plant():
+    try:
+        req = request.get_json()
+        with open("plant_data.json", "r") as file:
+            plant_data = json.load(file)
+        plant_data.append(req)
+        with open("plant_data.json", "w") as file:
+            file.write(json.dumps(plant_data, indent=4))
+        return "200 OK", 200
+    except:
+        return "Internal Server Error", 500
+
+@app.post("/api/search")
+def search_plants():
+    try:
+        req = request.get_json()
+        with open("plant_data.json", "r") as file:
+            plant_data = json.load(file)
+        print(plant_data)
+        return json.dumps(req, indent=4)
+    except:
+        return "Internal Server Error", 500
